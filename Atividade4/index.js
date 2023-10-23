07a7016b9710846bbef9a9262738b78c95c7ed8c
Gabriel Terra <gabrielterra07@gmail.com>
2023-10-23 00:20:46 -0300

    Criação da pasta da atividade

---
+++ Atividade4/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const calculadora = require('./util/calculadora');
+
+app.get('/somar/:a/:b', (req, res) => {
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+  const resultado = calculadora.somar(a, b);
+  res.send(`Resultado da soma: ${resultado}`);
+});
+
+app.get('/subtrair/:a/:b', (req, res) => {
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+  const resultado = calculadora.subtrair(a, b);
+  res.send(`Resultado da subtração: ${resultado}`);
+});
+
+app.get('/multiplicar/:a/:b', (req, res) => {
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+  const resultado = calculadora.multiplicar(a, b);
+  res.send(`Resultado da multiplicação: ${resultado}`);
+});
+
+app.get('/dividir/:a/:b', (req, res) => {
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+  const resultado = calculadora.dividir(a, b);
+  res.send(`Resultado da divisão: ${resultado}`);
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000');
+});
